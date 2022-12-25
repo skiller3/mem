d587b0412ac0e07a8f8ae63c8f47c8384cdb8477
Skye Isard <skye.isard@gmail.com>
2022-12-25 15:42:01 -0500

    Changed .bat and .sh scripts to correctly deal with file paths with whitespace

--- mem.bat
+++ mem.bat
@@ -1,4 +1,4 @@
 @echo off
-call %~dp0.venv-windows\Scripts\activate
-python %~dp0mem.py %*
+call "%~dp0.venv-windows\Scripts\activate"
+python "%~dp0mem.py" %*
 deactivate
--- mem.sh
+++ mem.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 SCRIPT=`readlink -f "$0"`
 SCRIPT_DIR=`dirname "$SCRIPT"`
-. $SCRIPT_DIR/.venv-ubuntu/bin/activate
-python mem.py "$@"
+. "$SCRIPT_DIR/.venv-ubuntu/bin/activate"
+python "$SCRIPT_DIR/mem.py" "$@"
 deactivate
