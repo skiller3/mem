1d1ab811de773095a5947c3e392349f4bd8fe92a
Skye Isard <skye.isard@gmail.com>
2023-01-21 16:43:36 -0500

    Fixed minor browser detection bug

--- web/static/memi.js
+++ web/static/memi.js
@@ -142,6 +142,7 @@
     }
 
     function getBrowser() {
+        let ua = navigator.userAgent;
         return /Edge\/\d+/.test(ua) ? 'ed' : /MSIE 9/.test(ua) ? 'ie9' : /MSIE 10/.test(ua) ? 'ie10' : /MSIE 11/.test(ua) ? 
                'ie11' : /MSIE\s\d/.test(ua) ? 'ie?' : /rv\:11/.test(ua) ? 'ie11' : /Firefox\W\d/.test(ua) ? 
                'ff' : /Chrom(e|ium)\W\d|CriOS\W\d/.test(ua) ? 'gc' : /\bSafari\W\d/.test(ua) ? 'sa' : /\bOpera\W\d/.test(ua) ?
